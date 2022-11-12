8cb27c330c27e061820a2c78f453cd2c1569687e
Ishanka Senevirathne <ishankadsenevirathne@gmail.com>
2022-11-12 23:46:33 +0530

    save world on local storage and reset world

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,11 @@ import { Player } from './components/Player';
 import { FPV } from './components/FPV';
 import { Cubes } from './components/Cubes';
 import { TextureSelector } from './components/TextureSelector';
+import { Menu } from './components/Menu';
 
 function App() {
   return (
     <>
-    <div>Outside Canvas</div>
     <Canvas>
       <Sky sunPosition={[100,100,20]}/>
       <ambientLight intensity={0.5}/>
@@ -23,6 +23,7 @@ function App() {
     </Canvas>
     <div className='absolute centered cursor'>+</div>
     <TextureSelector />
+    <Menu />
   </>
   )
 }
