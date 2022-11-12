66a7aad97d9bafcaad4c8e241249d045908c1389
Ishanka Senevirathne <ishankadsenevirathne@gmail.com>
2022-11-12 20:52:37 +0530

    adding a cursor to the view

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Physics } from '@react-three/cannon';
 import { Canvas } from '@react-three/fiber';
 import { Ground } from './components/Ground';
 import { Player } from './components/Player';
+import { FPV } from './components/FPV';
 
 function App() {
   return (
@@ -11,11 +12,13 @@ function App() {
     <Canvas>
       <Sky sunPosition={[100,100,20]}/>
       <ambientLight intensity={0.5}/>
+      <FPV />
       <Physics>
         <Player />
         <Ground />
       </Physics>
     </Canvas>
+    <div className='absolute centered cursor'>+</div>
   </>
   )
 }
