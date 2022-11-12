de26ba244c2e6f0ed765e676b3fd03be718c1a79
Ishanka Senevirathne <ishankadsenevirathne@gmail.com>
2022-11-12 17:49:53 +0530

    loading textures

---
+++ src/images/textures.ts
@@ -0,0 +1,26 @@
+import { TextureLoader } from 'three'
+
+import {
+    dirtImg,
+	grassImg,
+	glassImg,
+	woodImg,
+	logImg,
+} from './images'
+
+const dirtTexture = new TextureLoader(dirtImg);
+const logTexture = new TextureLoader(logImg);
+const grassTexture = new TextureLoader(grassImg);
+const woodTexture = new TextureLoader(woodImg);
+const glassTexture = new TextureLoader(glassImg);
+const groundTexture = new TextureLoader(grassImg);
+
+export {
+
+    dirtTexture,
+    logTexture,
+    grassTexture,
+    woodTexture,
+    glassTexture,
+    groundTexture
+}
