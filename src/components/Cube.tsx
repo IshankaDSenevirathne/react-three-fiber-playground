9da02261613402584d9ede50f885ccb3e526549c
Ishanka Senevirathne <ishankadsenevirathne@gmail.com>
2022-11-12 22:18:12 +0530

    adding adjecent cubes

--- src/components/Cube.tsx
+++ src/components/Cube.tsx
@@ -1,4 +1,5 @@
 import { useBox } from "@react-three/cannon";
+import { useStore } from "../hooks/useStore";
 import * as textures from '../images/textures';
 
 export const Cube = ({position, texture}) => {
@@ -7,9 +8,32 @@ export const Cube = ({position, texture}) => {
         position
     }))
 
-    const activeTexture = textures[texture+'Texture']
+    const [addCube, removeCube] = useStore((state)=>[state.addCube, state.removeCube]);
+
+    const activeTexture = textures[texture+'Texture'];
+
     return (
-        <mesh ref={ref}>
+        <mesh 
+            ref={ref}
+            onClick={(e)=>{
+                e.stopPropagation()
+                const clickedFace = Math.floor(e.faceIndex/2)
+                const {x, y, z} = ref.current.position
+                if(clickedFace === 0){
+                    addCube(x+1,y,z)
+                }else if(clickedFace === 1){
+                    addCube(x-1,y,z)
+                }else if(clickedFace === 2){
+                    addCube(x,y+1,z)
+                }else if(clickedFace === 3){
+                    addCube(x,y-1,z)
+                }else if(clickedFace === 4){
+                    addCube(x,y,z+1)
+                }else if(clickedFace === 5){
+                    addCube(x,y,z-1)
+                }
+            }}
+        >
             <boxBufferGeometry attach='geometry' />
             <meshStandardMaterial map={activeTexture} attach='material' />
         </mesh>
